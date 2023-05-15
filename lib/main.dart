696a7e1ca49fe9291960d1e10570f47932f95280
ramdanisalah67 <ramdanisalah67@gmail.com>
2023-05-15 02:53:18 +0200

    part 1 widgets

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('this is Title'),
+          backgroundColor: Color.fromARGB(255, 18, 153, 36),
+        ),
+      ),
+    ),
+  );
+}
